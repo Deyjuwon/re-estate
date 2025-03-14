fd6ae7a56fc172a25efce2444350ce53dadd454c
Deyjuwon <temitayoadejuwon@gmail.com>
2025-03-14 19:33:08 +0100

    first commit

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="font-bold text-red-600 my-10">welcome</Text>
+      <Link href='/sign-in'>Sign In</Link>
+      <Link href='/explore'>Explore</Link>
+      <Link href='/profile'>Profile</Link>
+      <Link href='/properties/1'>Property</Link>
+    </View>
+  );
+}
