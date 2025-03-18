85eb65bb9d7c8443dc36ae021d6fe30c586078b8
Deyjuwon <temitayoadejuwon@gmail.com>
2025-03-18 08:48:53 +0100

    sign in page

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -9,10 +9,12 @@ export default function Index() {
         justifyContent: "center",
         alignItems: "center",
       }}
-      className="bg-primary-200"
+      className="bg-primary-200 "
+    
     >
-      <Text className="font-bold text-red-600 my-10">welcome</Text>
-      <Link href='/sign-in'>Sign In</Link>
+      <Text className=" text-red-600 my-10 font-rubik-light text-3xl">welcome to ReState</Text>
+      <Text className=" text-red-600 my-10 text-3xl">welcome to ReState</Text>
+      <Link href='/sign-in' className="">Sign In</Link>
       <Link href='/explore'>Explore</Link>
       <Link href='/profile'>Profile</Link>
       <Link href='/properties/1'>Property</Link>
